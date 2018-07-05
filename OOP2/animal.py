d56a5905beafb2f112477802ff22ddf5ba5df6d7
Gauravsahadev <gauravsahadeo6@gmail.com>
2018-07-05 15:31:59 +0530

    animal.py modified

--- OOP2/animal.py
+++ OOP2/animal.py
@@ -3,7 +3,15 @@
 
 class Animal:
     pass
-    cool = True
+
+    def __init__(self, name, species):
+        pass
+        self.name = name
+        self.species = species
+
+    def __repr__(self):
+        pass
+        return "{} is a {} ".format(self.name, self.species)
 
     def make_sound(self, sound):
         pass
@@ -13,7 +21,26 @@ class Animal:
 class Cat(Animal):
     pass
 
+    def __init__(self, name, species, breed, favorite_toy):
+        pass
+        # Animal.__init__(self,name,species)
+        super().__init__(name, species)
+        self.breed = breed
+        self.favorite_toy = favorite_toy
 
-temp = Cat()
-temp.make_sound("Meow")
-print(temp.cool)
+
+temp = Cat("kitty", "Cat", "Scotish Fold", "string")
+print(temp)
+print(temp.species, temp.favorite_toy)
+
+
+# OUR "MODEL" FOR ANIMAL AND CAT
+# Animal
+# 	species
+# 	name
+
+# Cat
+# 	species
+# 	name
+# 	breed
+# 	favorite_toy
